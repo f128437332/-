daadb4080a95af32cdf569711c8b53fa94936401
WANG JYUN <f128437332@gmail.com>
2018-09-18 20:02:14 +0800

    first commit

---
+++ 1.py
@@ -0,0 +1,20 @@
+#int 整數
+#float 小數
+#bool 布林值，只有真假
+#str 字串，'你好棒棒'
+#input輸入的意思
+
+#echo "# -" >> README.md
+#git init (初始化)
+#git add README.md (將檔案加入追蹤清單)
+#git commit -m "first commit"(建立版本)
+#git remote add origin https://github.com/f128437332/-.git(跟GIT產生連結)
+#git push -u origin master(上傳)
+
+
+
+
+
+name = input
+input('請問身高體重為多少： ')
+print('好的謝謝')
